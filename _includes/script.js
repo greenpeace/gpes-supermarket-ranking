9bbb69585f5ab0681e334692357be4ed19019143
Osvaldo Gago <opgago@yahoo.com>
2018-12-19 10:09:51 +0100

    Arrow left

--- _includes/script.js
+++ _includes/script.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("rankingArrowRight").addEventListener("click", function(){
         document.getElementById('table-responsive').scrollLeft += 40;
     });
+    document.getElementById("rankingArrowLeft").addEventListener("click", function(){
+        document.getElementById('table-responsive').scrollLeft += -40;
+    });
 });
