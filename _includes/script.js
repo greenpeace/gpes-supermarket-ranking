166af369898792986ab537d56c1be6d1d6285026
Osvaldo Gago <opgago@yahoo.com>
2018-12-23 11:08:29 +0000

    Add comments

--- _includes/script.js
+++ _includes/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
+    /* BEGIN INTERACTIVITY */
     document.getElementById("rankingArrowRight").addEventListener("click", function(){
         document.getElementById('table-responsive').scrollLeft += 40;
     });
     document.getElementById("rankingArrowLeft").addEventListener("click", function(){
         document.getElementById('table-responsive').scrollLeft += -40;
     });
+    /* END INTERACTIVITY */
 });
